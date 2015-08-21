d6a0c06e9f9e70e7cb9e8d994b73485ae5d44e27
unknown <ducdnm@technew-ducdnm.fo.com>
2015-08-21 14:10:28 +0700

    Adding comments for exported functions and properties

--- concache.go
+++ concache.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	NO_EXPIRATION    time.Duration = -1
-	CLEANUP_INTERVAL               = 60
+	CLEANUP_INTERVAL               = 60 // interval time for cleaning expired items
 )
 
 type Item struct {
@@ -23,6 +23,7 @@ type Item struct {
 	ExpiredTime *time.Time
 }
 
+// ConcurrentMap, Item and their properties are exported because of using GOB to persist contents to file and to reload them again
 type ConcurrentMap struct {
 	Items map[string]*Item
 }
@@ -36,6 +37,9 @@ type Cache struct {
 	cacheName string
 }
 
+// Create new cache instance with:
+// shardedMapsCount: how many sharded maps in this cache instance
+// cacheName: name of this cache, this wll be useful for cache persistence to disk
 func NewCache(shardedMapsCount int, cacheName string) *Cache {
 	max := big.NewInt(0).SetUint64(uint64(math.MaxUint32))
 	rnd, err := rand.Int(rand.Reader, max)
@@ -64,6 +68,7 @@ func NewCache(shardedMapsCount int, cacheName string) *Cache {
 	return cacheInstance
 }
 
+// djb hash algorithms
 func djbHasher(seed uint32, k string) uint32 {
 	var (
 		l = uint32(len(k))
@@ -95,12 +100,15 @@ func djbHasher(seed uint32, k string) uint32 {
 	return d ^ (d >> 16)
 }
 
+// Get bucket (or sharded map) by key in 1 cache instance with djb hash algorithms
+// By using djb hash algorithms, this function should be 5 times faster than using built in hash function of Golang
 func (cache *Cache) getBucketWithDjbHasher(key string) (*ConcurrentMap, uint32) {
 	bucketsCount := (uint32)(len(cache.buckets))
 	bucketIndex := djbHasher(cache.seed, key) % bucketsCount
 	return cache.buckets[bucketIndex], bucketIndex
 }
 
+// Get bucket (or sharded map) by key in 1 cache instance with built in hash function of Golang
 func (cache *Cache) getBucketWithBuiltInHasher(key string) (*ConcurrentMap, uint) {
 	hasher := fnv.New32()
 	hasher.Write([]byte(key))
@@ -109,6 +117,7 @@ func (cache *Cache) getBucketWithBuiltInHasher(key string) (*ConcurrentMap, uint
 	return cache.buckets[bucketIndex], bucketIndex
 }
 
+// Get an item from cache by key. Return the item value (if found & not expired yet). Otherwise, return nil. And bool indicates whether key was found or not.
 func (cache *Cache) Get(key string) (interface{}, bool) {
 	bucket, _ := cache.getBucketWithDjbHasher(key)
 
@@ -122,6 +131,7 @@ func (cache *Cache) Get(key string) (interface{}, bool) {
 	return item.Value, true
 }
 
+// Set an item to the cache with given key and replace any existing item. If expiredDuration is -1 (NO_EXPIRATION) then the item will never be expired.
 func (cache *Cache) Set(key string, value interface{}, expiredDuration time.Duration) {
 	var expiredTime *time.Time
 	if expiredDuration > -1 {
@@ -139,6 +149,7 @@ func (cache *Cache) Set(key string, value interface{}, expiredDuration time.Dura
 	cache.Unlock()
 }
 
+// Delete an item from the cache by key and do nothing if the key is not found
 func (cache *Cache) Delete(key string) {
 	bucket, _ := cache.getBucketWithDjbHasher(key)
 	cache.Lock()
@@ -146,6 +157,7 @@ func (cache *Cache) Delete(key string) {
 	cache.Unlock()
 }
 
+// Check an item is expired or not
 func IsExpired(item *Item) bool {
 	if item.ExpiredTime == nil {
 		return false
@@ -153,6 +165,7 @@ func IsExpired(item *Item) bool {
 	return item.ExpiredTime.Before(time.Now())
 }
 
+// Manually delete expired items in all buckets (sharded maps) of the cache
 func (cache *Cache) DeleteExpiredItems() {
 	buckets := cache.buckets
 	for _, bucket := range buckets {
@@ -166,6 +179,7 @@ func (cache *Cache) DeleteExpiredItems() {
 	}
 }
 
+// cleaner will run and delete all expired items at time-interval (CLEANUP_INTERVAL = 60s)
 func (cache *Cache) runExpiredItemCleaner() {
 	ticker := time.NewTicker(time.Second * CLEANUP_INTERVAL).C
 	go func() {
@@ -178,6 +192,7 @@ func (cache *Cache) runExpiredItemCleaner() {
 	}()
 }
 
+// Return all items (may include expired items that has not been deleted by cleaner yet) in the cache
 func (cache *Cache) GetAllItemsCache() map[string]*Item {
 	allItems := make(map[string]*Item)
 	buckets := cache.buckets
@@ -191,6 +206,7 @@ func (cache *Cache) GetAllItemsCache() map[string]*Item {
 	return allItems
 }
 
+// Return count of all items (may include expired items that has not been deleted by cleaner yet) in the cache
 func (cache *Cache) CountItemsCache() int {
 	buckets := cache.buckets
 	numItems := 0
@@ -202,6 +218,7 @@ func (cache *Cache) CountItemsCache() int {
 	return numItems
 }
 
+// Delete all items in the cache
 func (cache *Cache) FlushAll() {
 	buckets := cache.buckets
 	for _, bucket := range buckets {
@@ -218,6 +235,7 @@ func checkError(err error) {
 	}
 }
 
+// Save data of the cache to file on disk. File's name is cache's name
 func (cache *Cache) saveCacheToFile() {
 	buckets := cache.buckets
 	cacheFile, err := os.Create(cache.cacheName + ".txt")
@@ -228,10 +246,12 @@ func (cache *Cache) saveCacheToFile() {
 	dataEncoder.Encode(buckets)
 }
 
+// Persist data of the cache to file on disk. File's name is cache's name. It's useful for reloading those data again (ex: when restart server ...)
 func (cache *Cache) PersistCacheToDisk() {
 	cache.saveCacheToFile()
 }
 
+// Execute backup (aka: save content of the cache to file) at given time-interval
 func (cache *Cache) BackUpCacheToDiskInterval(backUpInterval time.Duration) {
 	ticker := time.NewTicker(backUpInterval).C
 	go func() {
@@ -244,6 +264,8 @@ func (cache *Cache) BackUpCacheToDiskInterval(backUpInterval time.Duration) {
 	}()
 }
 
+// Load all items that were persisted to file on disk. File's name is cache's name.
+// An important note, to load successfully content from file to buckets of the cache, count of cache's buckets should be equal count of buckets that was loaded from file
 func (cache *Cache) LoadCacheFromDisk() {
 	cacheFile, err := os.Open(cache.cacheName + ".txt")
 	defer cacheFile.Close()
